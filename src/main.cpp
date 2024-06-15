c7bcbf42c113686ce7b286dc266ed361216de6c5
Ian Renton <ian@ianrenton.com>
2024-06-15 16:40:55 +0100

    Add mutex for copying data between tasks, debug LED flash, general tidyup

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,17 @@
 
 // Timing
 #define MOUTH_FLAP_INTERVAL_MILLIS 100 // Rate of mouth movement. Lower number = faster
+#define SERIAL_BAUD 115200
+
+// FFT parameters. Hopefully these match the sampling frequency and number of samples per packet
+// coming from your device. This is what works with my phone!
+#define SAMPLING_FREQUENCY 44100
+#define SAMPLES_PER_FFT 2048
+
+// I2S pins
+#define I2S_BCLK_PIN 18
+#define I2S_WS_PIN 19
+#define I2S_DOUT_PIN 21
 
 // Motor control pins
 #define HEADTAIL_MOTOR_PIN_1 12
@@ -16,6 +27,10 @@
 #define MOUTH_MOTOR_PIN_1 27
 #define MOUTH_MOTOR_PIN_2 26
 #define MOUTH_MOTOR_PWM_PIN 25
+
+// Misc pins
+#define LED_PIN 2
+#define BUTTON_PIN 4
 
 // Motor PWM settings
 #define PWM_FREQUENCY 1000
@@ -42,27 +57,26 @@ void mouthOpen();
 void mouthClose();
 void mouthRest();
 
-// FFT parameters. Hopefully these match the sampling frequency and number of samples per packet
-// coming from your device. This is what works on my phone!
-const double samplingFrequency = 44100;
-const int numSamples = 2048;
-
 // Data storage for FFT, FFT object & task
-double vReal[numSamples];
-double vImag[numSamples];
-ArduinoFFT<double> fft = ArduinoFFT<double>(vReal, vImag, numSamples, samplingFrequency);
+double vReal[SAMPLES_PER_FFT];
+double vImag[SAMPLES_PER_FFT];
+ArduinoFFT<double> fft = ArduinoFFT<double>(vReal, vImag, SAMPLES_PER_FFT, SAMPLING_FREQUENCY);
+
+// Data storage & mutex for passing audio samples between CPUs
+double audioTransferBuffer[SAMPLES_PER_FFT];
+SemaphoreHandle_t mutex = xSemaphoreCreateMutex();
 
 // Create Bluetooth autio receiver
 btAudio audio = btAudio(DEVICE_NAME);
-
-// For inter-process communication, track whether we are receiving audio currently
-boolean receivingAudio = false;
 
 // Setup and run the program
 void setup()
 {
   // Set up serial
-  Serial.begin(115200);
+  Serial.begin(SERIAL_BAUD);
+
+  // Set up debug LED
+  pinMode(LED_PIN, OUTPUT);
 
   // Set up motor control pins
   pinMode(HEADTAIL_MOTOR_PIN_1, OUTPUT);
@@ -87,10 +101,7 @@ void setup()
   audio.reconnect();
 
   // Set up streaming audio over I2S
-  int bck = 26;
-  int ws = 27;
-  int dout = 25;
-  audio.I2S(bck, dout, ws);
+  audio.I2S(I2S_BCLK_PIN, I2S_DOUT_PIN, I2S_WS_PIN);
 
   // Set our custom sink callback; this will pass audio data to I2S but also store the
   // data for FFT
@@ -101,9 +112,18 @@ void setup()
 // moves the fish motors accordingly.
 void loop()
 {
+  // Copy data out of the inter-process audio transfer buffer into the "real" data buffer
+  // that will be used for the FFT. This is controlled with a mutex lock to ensure we
+  // don't read from it and write to it at the same time.
+  xSemaphoreTake(mutex, portMAX_DELAY);
+  for (int i = 0; i < SAMPLES_PER_FFT; i++)
+  {
+    vReal[i] = audioTransferBuffer[i];
+  }
+  xSemaphoreGive(mutex);
   // Zero out the imaginary data buffer, leaving only the real data buffer written to
   // by the bluetooth-to-I2S callback
-  for (int i = 0; i < numSamples; i++)
+  for (int i = 0; i < SAMPLES_PER_FFT; i++)
   {
     vImag[i] = 0.0;
   }
@@ -115,8 +135,7 @@ void loop()
   boolean receivingAudio = !isnan(fft.majorPeak());
 
   // Work out if we are receiving audio. If so, stick the fish head out and flap the mouth,
-  // if not bring it in.
-  Serial.println(receivingAudio); // @todo remove
+  // if not, then rest both motors.
   if (receivingAudio)
   {
     headOut();
@@ -136,21 +155,31 @@ void loop()
 // data to I2S but also populates a second buffer for computing the FFT.
 void sinkCallback(const uint8_t *data, uint32_t len)
 {
-  int numSamples = len / 2;
+  int samplesReceived = len / 2;
   // Handle input as 16-bit data
-  int16_t *data16 = (int16_t *)data;
+  int16_t* data16 = (int16_t *)data;
 
+  // Write to I2S
   size_t i2s_bytes_write = 0;
-  for (int i = 0; i < numSamples; i++)
+  for (int i = 0; i < samplesReceived; i++)
   {
-    // Write to I2S
     i2s_write(I2S_NUM_0, data16, 2, &i2s_bytes_write, 10);
-
-    // Write to FFT data buffer
-    vReal[i] = *data16;
-
     data16++;
   }
+
+  // Reset data pointer
+  data16 = (int16_t *)data;
+
+  // Write to the audio transfer buffer to pass this data over to the FFT task on
+  // the other CPU. This is controlled with a mutex lock to ensure we don't read
+  // from it and write to it at the same time.
+  xSemaphoreTake(mutex, portMAX_DELAY);
+  for (int i = 0; i < min(samplesReceived, SAMPLES_PER_FFT); i++)
+  {
+    audioTransferBuffer[i] = *data16;
+    data16++;
+  }
+  xSemaphoreGive(mutex);
 }
 
 // Bring the fish's head out
@@ -181,6 +210,7 @@ void mouthOpen()
 {
   digitalWrite(MOUTH_MOTOR_PIN_1, LOW);
   digitalWrite(MOUTH_MOTOR_PIN_2, HIGH);
+  digitalWrite(LED_PIN, HIGH); // Debug
 }
 
 // Close the fish's mouth
@@ -188,6 +218,7 @@ void mouthClose()
 {
   digitalWrite(MOUTH_MOTOR_PIN_1, HIGH);
   digitalWrite(MOUTH_MOTOR_PIN_2, LOW);
+  digitalWrite(LED_PIN, LOW); // Debug
 }
 
 // Rest the fish's mouth
@@ -195,4 +226,5 @@ void mouthRest()
 {
   digitalWrite(MOUTH_MOTOR_PIN_1, LOW);
   digitalWrite(MOUTH_MOTOR_PIN_2, LOW);
+  digitalWrite(LED_PIN, LOW); // Debug
 }
